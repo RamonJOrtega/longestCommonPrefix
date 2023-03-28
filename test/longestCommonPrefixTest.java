4351e195921a530292c739c8dfb2d719a451547a
ramon6767 <ramon.justis.ortega@gmail.com>
2023-03-28 10:19:06 -0700

    create src and test ffolders and put longestCommonPrefix.java and longestCommonPrefix.java in correct folders

---
+++ test/longestCommonPrefixTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import src.longestCommonPrefix;
+
+
+
+public class longestCommonPrefixTest {
+    private longestCommonPrefix longestCommonPrefix;
+
+    @Test
+    public void testGetLongestCommonPrefix() {
+        String[] input = {"flower","flow","flight"};
+        String expectedOutput = "fl";
+        String result = longestCommonPrefix.getLongestCommonPrefix(input);
+        assertEquals(expectedOutput, result);
+    }
+}
