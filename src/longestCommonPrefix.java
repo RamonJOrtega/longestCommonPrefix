542bb3b0f4f5445188131b5b679e183ca1c504fe
ramon6767 <ramon.justis.ortega@gmail.com>
2023-03-28 14:58:10 -0700

    revers test criteria

--- src/longestCommonPrefix.java
+++ src/longestCommonPrefix.java
@@ -3,11 +3,26 @@ package src;
  * longestCommonPrefix
  */
 public class longestCommonPrefix {
+
     public String getLongestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
 
-        String longestCommonPrefix = "";
-        return longestCommonPrefix;
+        String prefix = strs[0];
+
+        for (int i = 0; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() -1);
+            }
+
+            if (prefix.isEmpty()) {
+                return "";
+            }
+            System.out.println(prefix);
+        }
+        
+        return prefix;
     }
-
     
 }
--- test/longestCommonPrefixTest.java
+++ test/longestCommonPrefixTest.java
@@ -11,17 +11,19 @@ public class longestCommonPrefixTest {
     private longestCommonPrefix longestCommonPrefix;
 
     @Test
-    public void testIsCommonPrefix() {
-        String[] input = {"dog", "racecar", "flight"};
-        String expectedOutput ="";
-        String result = longestCommonPrefix.getLongestCommonPrefix(input);
-        assertEquals(expectedOutput, result);
-    }
+
     public void testGetLongestCommonPrefix() {
         String[] input = {"flower","flow","flight"};
         String expectedOutput = "fl";
         String result = longestCommonPrefix.getLongestCommonPrefix(input);
         assertEquals(expectedOutput, result);
     }
+
+    @Test
+    public void testIsCommonPrefix() {
+        String[] input = {"dog", "racecar", "flight"};
+        String expectedOutput ="";
+        String result = longestCommonPrefix.getLongestCommonPrefix(input);
+        assertEquals(expectedOutput, result);
+    }
 }
-s
